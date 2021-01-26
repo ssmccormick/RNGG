b397035b9e396b45b63c55ae2644a6d92c26d50e
ssmccormick <steeleman8@gmail.com>
2021-01-26 13:58:57 -1000

    Reactifying The Card Deck tutorial

--- rngg/src/App.js
+++ rngg/src/App.js
@@ -1,57 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import DeckApp from './deck';
 
 
-let suits =["spades", "diamonds", "clubs", "hearts"];
-let values =["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-function getDeck() {
-  let deck = new Array();
-  for(let i = 0; i < suits.length; i++) {
-    for(let x = 0; x < values.length; x++) {
-      let card = {Value: values[x], Suit: suits[i]};
-      deck.push(card);
-    }
-  }
-    return deck;
-}
-
-function shuffle(deck) {
-  for (let i = 0; i < 1000; i++) {
-    let location1 = Math.floor((Math.random() * deck.length));
-    let location2 = Math.floor((Math.random() * deck.length));
-    let tmp = deck[location1];
-
-    deck[location1] = deck[location2];
-    deck[location2] = tmp;
-  }
-}
-
-function renderDeck(deck) {
-    document.getElementById("deck").innerHTML = "";
-
-  for(let i = 0; i < deck.length; i++) {
-    let card = document.createElement("div");
-    let icon = '';
-    if (deck[i].Suit == 'hearts'){
-      icon = '?';
-    } else if (deck[i].Suit == 'spades') {
-      icon = '?';
-    } else if (deck[i].Suit == 'diamonds') {
-      icon = '?';
-    } else if (deck[i].Suit == 'clubs') {
-      icon = '?';
-    } else {
-      icon = '?';
-    }
-    card.innerHTML = deck[i].Value + '' + icon;
-    card.className = 'card';
-  document.getElementById("deck").appendChild(card);  
-  }
-}
-
-
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -68,7 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <div id="deck">{renderDeck()}</div>
+      <DeckApp />
     </div>
   );
 }
---
+++ rngg/src/deck/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+
+
+const Deck = ({ deck }) => {
+
+  for(let i = 0; i < deck.length; i++) {
+    let card = <div>{deck[i].Value}{deck[i].Suit}</div>
+    let icon = '';
+    if (deck[i].Suit === 'hearts'){
+      icon = '♥';
+    } else if (deck[i].Suit === 'spades') {
+      icon = '♠';
+    } else if (deck[i].Suit === 'diamonds') {
+      icon = '♦';
+    } else {
+      icon = '♣';
+    }
+    return (
+      <div>
+        {card}
+      </div>
+  );
+  }
+}
+
+const ShuffleDeck = (deck) => {
+
+  for (let i = 0; i < 1000; i++) {
+    let location1 = Math.floor((Math.random() * deck.length));
+    let location2 = Math.floor((Math.random() * deck.length));
+    let temp = deck[location1];
+
+    deck[location1] = deck[location2];
+    deck[location2] = temp;
+  }
+
+  return (deck)
+}
+
+
+const DeckApp = () => {
+
+  const getDeck = () => {
+
+    let suits =["spades", "diamonds", "clubs", "hearts"];
+    let values =["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let deck = new Array();
+    for(let i = 0; i < suits.length; i++) {
+      for(let x = 0; x < values.length; x++) {
+        let card = {Value: values[x], Suit: suits[i]};
+        deck.push(card);
+      }
+    return (deck);
+    }
+  }
+
+  return (
+    <div className="deck-contain">
+      <h1>A Shuffled Deck of Cards</h1>
+      <Deck deck={ShuffleDeck()}/>
+      <ShuffleDeck deck={getDeck()} />
+    </div>
+  );
+}
+
+export default DeckApp;
